5538ba563c51c1c206fb2e2c96c9f355c52080eb
hanniel09 <hannielvieira1227@gmail.com>
2023-03-29 00:25:45 -0300

    first commit

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,13 @@ module.exports = {
     singleQuote: true,
     printWidth: 80,
     tabWidth: 2,
+  },
+  {
+    "prettier/prettier": [
+      "error",
+      {},
+      {
+        "usePrettierrc": false
+      }
+    ]
   }
