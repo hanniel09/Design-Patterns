d4e7781dff524a3ffd4329d0f43592150d0b0a7f
hanniel09 <hannielvieira1227@gmail.com>
2023-03-31 16:08:57 -0300

    feature

---
+++ src/creational/builder/index.ts
@@ -0,0 +1,9 @@
+import { MealBox } from './classes/meal-box';
+import { Beans, Meat, Rice } from './classes/meals';
+
+const rice = new Rice('Arroz', 5);
+const beans = new Beans('Feijão', 10);
+const meat = new Meat('Carne', 20);
+const mealBox = new MealBox();
+mealBox.add(rice, beans, meat);
+console.log(mealBox);
