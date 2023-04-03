384994199f01782ecdb09092858720963ba8ffdd
hanniel09 <hannielvieira1227@gmail.com>
2023-04-03 09:49:30 -0300

    Feature

--- src/creational/builder/classes/vegan-dish-builder.ts
+++ src/creational/builder/classes/vegan-dish-builder.ts
@@ -1,6 +1,6 @@
 import { MealBuilderProtocol } from '../interfaces/meal-builder-protocol';
 import { MealBox } from './meal-box';
-import { Rice, Beans, Beverage, Dessert } from './meals';
+import { Rice, Beans } from './meals';
 
 export class VeganDishBuilder implements MealBuilderProtocol {
   private _meal: MealBox = new MealBox();
